52b8a49e650afa941929a77a11c372fde25ba5d5
Catalin Marinas <catalin.marinas@arm.com>
2007-02-02 10:14:49 +0000

    run_init_process() error reporting
    This function prints the error code returned by execve.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- init/main.c
+++ init/main.c
@@ -701,8 +701,11 @@ static void do_pre_smp_initcalls(void)
 
 static void run_init_process(char *init_filename)
 {
+	long ret;
+
 	argv_init[0] = init_filename;
-	kernel_execve(init_filename, argv_init, envp_init);
+	ret = kernel_execve(init_filename, argv_init, envp_init);
+	printk(KERN_INFO "%s exit code: %ld\n", init_filename, ret);
 }
 
 static int init(void * unused)
