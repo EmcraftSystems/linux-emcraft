77b7395be33778031af50244b0af536d0c8d60f7
Catalin Marinas <catalin.marinas@arm.com>
2009-03-10 10:24:28 +0000

    nommu: Add MMU-less support for RealView/EB
    This patch redefines the IO_ADDRESS macro.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/mach-realview/include/mach/hardware.h
+++ arch/arm/mach-realview/include/mach/hardware.h
@@ -32,7 +32,11 @@
  * 1exx xxxx -> fdxx xxxx
  * 1fxx xxxx -> fexx xxxx
  */
+#ifdef CONFIG_MMU
 #define IO_ADDRESS(x)		(((x) & 0x03ffffff) + 0xfb000000)
+#else
+#define IO_ADDRESS(x)		(x)
+#endif
 #define __io_address(n)		__io(IO_ADDRESS(n))
 
 #endif
