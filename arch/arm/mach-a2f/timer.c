24deeac75507abd1056a9df816e557988d0077da
Sergei Poselenov <sposelenov@emcraft.com>
2011-01-29 20:07:53 +0300

    RT #62663. Fixed system initialization.

--- arch/arm/mach-a2f/timer.c
+++ arch/arm/mach-a2f/timer.c
@@ -71,13 +71,7 @@ static void mss_timer1_set_mode(enum clock_event_mode mode,
 		 * Kernel ticker rate. Timer1 is clocked from PCLK0.
 		 * TO-DO: is the Linux system clock rate configurable?
 		 */
-#if 0
-		/* Actel A2F EVB, 100MHz coreclock */
-		MSS_TIMER1->loadval= a2f_clock_get(CLCK_SYSTEMCORE) / 100;
-#else
-		/* Emcraft A2F-LNX-EVB, 80MHz coreclock */
-		MSS_TIMER1->loadval= a2f_clock_get(CLCK_SYSTEMCORE) / 125;
-#endif
+		MSS_TIMER1->loadval = a2f_clock_get(CLCK_PCLK0) / HZ;
 		/*
 		 * Enable interrupts, Periodic Mode, Timer Enabled
 		 */
