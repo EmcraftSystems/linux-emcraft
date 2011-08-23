44c0fa9efea6d7fae9e6232a6adfff5fd7126275
Dmitry Konyshev <probables@ocean.emcraft.com>
2011-08-23 16:35:45 +0400

    allow read/write to /dev/mem at SYSREG offset

--- arch/arm/mach-a2f/include/mach/io.h
+++ arch/arm/mach-a2f/include/mach/io.h
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-a2f/include/mach/io.h
  *
- * Copyright (C) 2009 ARM Ltd.
+ * Copyright (C) 2010-2011 EmCraft Systems
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -19,6 +19,9 @@
 #ifndef __ASM_ARM_ARCH_IO_H
 #define __ASM_ARM_ARCH_IO_H
 
+#include <mach/a2f.h>
+#include <linux/mm.h>
+
 #define IO_SPACE_LIMIT 0xffffffff
 
 static inline void __iomem *__io(unsigned long addr)
@@ -28,5 +31,29 @@ static inline void __iomem *__io(unsigned long addr)
 
 #define __io(a)                 __io(a)
 #define __mem_pci(a)            (a)
+
+#define ARCH_HAS_VALID_PHYS_ADDR_RANGE
+
+#ifndef __ASSEMBLY__
+
+static inline int valid_phys_addr_range(unsigned long addr, size_t size)
+{
+	if (addr + size <= __pa(high_memory))
+		return 1;
+
+	/* allow user programs to read/write SYSREG */
+	if (addr >= A2F_SYSREG_BASE &&
+		addr + size < A2F_SYSREG_BASE + sizeof(struct a2f_sysreg))
+		return 1;
+	
+	return 0;
+}
+
+static inline int valid_mmap_phys_addr_range(unsigned long pfn, size_t size)
+{
+	return 1;
+}
+
+#endif
 
 #endif
