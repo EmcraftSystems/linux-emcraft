422bded6fbf6388c9fe50a409182f82688a08183
Sergei Poselenov <sposelenov@emcraft.com>
2013-11-15 17:49:41 +0400

    RT #84677. K70 sdhc: Added similar "card detect" pin configuration to TWR-K70F120.

--- arch/arm/mach-kinetis/iomux.c
+++ arch/arm/mach-kinetis/iomux.c
@@ -239,8 +239,8 @@ static const struct kinetis_gpio_pin_config twrk70f120m_iomux[] = {
 	/* E.5 = SDHC0_D2 */
 	{{KINETIS_GPIO_PORT_E, 5}, KINETIS_GPIO_CONFIG_MUX(4)},
 	/* E.28 = GPIO (Card detect) */
-//	{{KINETIS_GPIO_PORT_E, 28}, KINETIS_GPIO_CONFIG_MUX(1) | (0xb << 16) | 3},
-	{{KINETIS_GPIO_PORT_E, 28}, KINETIS_GPIO_CONFIG_MUX(1) | (0xb << 16)},
+	{{KINETIS_GPIO_PORT_E, 28}, KINETIS_GPIO_CONFIG_PULLUP(1) |
+	 KINETIS_GPIO_CONFIG_IRQC_EITHER},
 #endif
 };
 
