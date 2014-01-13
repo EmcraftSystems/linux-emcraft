409c1b459a67ed3601798251854a7c510497fc31
Axel Utech <axel.utech@gmail.com>
2014-01-13 15:02:53 +0100

    ARM: Kinetis: Separate config option to enable the USB-FS hardware
    The hardware module is used by host and gadget usb drivers. This option
    enables the initialization for the module and its clock during startup.
    
    
    Signed-off-by: Axel Utech <axel.utech@gmail.com>

--- arch/arm/mach-kinetis/kinetis_platform.c
+++ arch/arm/mach-kinetis/kinetis_platform.c
@@ -259,7 +259,7 @@ static void __init kinetis_init(void)
 	kinetis_ehci_init();
 #endif
 
-#if defined(CONFIG_USB_KHCI_HCD)
+#if defined(CONFIG_KINETIS_USBFS)
 	/*
 	 * Configure the Freescale Kinetis USB Full Speed controller
 	 */
