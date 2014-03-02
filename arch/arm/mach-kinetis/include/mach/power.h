7c9c23223f5d316d40a1b5d93fee793eb2a5fc24
Vladimir Khusainov <vlad@emcraft.com>
2014-03-02 13:43:06 +0400

    RT79435 Support all clock gates in kinetis_periph_enable(). Add a fucntion to print the clock gates settings.

--- arch/arm/mach-kinetis/include/mach/power.h
+++ arch/arm/mach-kinetis/include/mach/power.h
@@ -54,45 +54,66 @@ typedef u32 kinetis_clock_gate_t;
 /*
  * Clock gates for the modules inside the MCU
  */
-/* UARTs */
-#define KINETIS_CG_UART0	KINETIS_MKCG(3, 10)	/* SIM_SCGC4[10] */
-#define KINETIS_CG_UART1	KINETIS_MKCG(3, 11)	/* SIM_SCGC4[11] */
-#define KINETIS_CG_UART2	KINETIS_MKCG(3, 12)	/* SIM_SCGC4[12] */
-#define KINETIS_CG_UART3	KINETIS_MKCG(3, 13)	/* SIM_SCGC4[13] */
-#define KINETIS_CG_UART4	KINETIS_MKCG(0, 10)	/* SIM_SCGC1[10] */
-#define KINETIS_CG_UART5	KINETIS_MKCG(0, 11)	/* SIM_SCGC1[11] */
-/* Ports */
-#define KINETIS_CG_PORTA	KINETIS_MKCG(4, 9)	/* SIM_SCGC5[9] */
-#define KINETIS_CG_PORTB	KINETIS_MKCG(4, 10)	/* SIM_SCGC5[10] */
-#define KINETIS_CG_PORTC	KINETIS_MKCG(4, 11)	/* SIM_SCGC5[11] */
-#define KINETIS_CG_PORTD	KINETIS_MKCG(4, 12)	/* SIM_SCGC5[12] */
-#define KINETIS_CG_PORTE	KINETIS_MKCG(4, 13)	/* SIM_SCGC5[13] */
-#define KINETIS_CG_PORTF	KINETIS_MKCG(4, 14)	/* SIM_SCGC5[14] */
-/* ENET */
-#define KINETIS_CG_ENET		KINETIS_MKCG(1, 0)	/* SIM_SCGC2[0] */
-/* Periodic Interrupt Timer (PIT) */
-#define KINETIS_CG_PIT		KINETIS_MKCG(5, 23)	/* SIM_SCGC6[23] */
-/* LCD Controller */
-#define KINETIS_CG_LCDC		KINETIS_MKCG(2, 22)	/* SIM_SCGC3[22] */
-/* DMA controller and DMA request multiplexer */
-#define KINETIS_CG_DMA		KINETIS_MKCG(6, 1)	/* SIM_SCGC7[1] */
-#define KINETIS_CG_DMAMUX0	KINETIS_MKCG(5, 1)	/* SIM_SCGC6[1] */
-#define KINETIS_CG_DMAMUX1	KINETIS_MKCG(5, 2)	/* SIM_SCGC6[2] */
-/* USB High Speed */
-#define KINETIS_CG_USBHS	KINETIS_MKCG(5, 20)	/* SIM_SCGC6[20] */
-/* USB Full Speed */
-#define KINETIS_CG_USBFS	KINETIS_MKCG(3, 18)	/* SIM_SCGC4[18] */
-/* ADC modules */
-#define KINETIS_CG_ADC0		KINETIS_MKCG(5, 27)	/* SIM_SCGC6[27] */
-#define KINETIS_CG_ADC1		KINETIS_MKCG(2, 27)	/* SIM_SCGC3[27] */
-#define KINETIS_CG_ADC2		KINETIS_MKCG(5, 28)	/* SIM_SCGC6[28] */
-#define KINETIS_CG_ADC3		KINETIS_MKCG(2, 28)	/* SIM_SCGC3[28] */
-/* ESDHC */
-#define KINETIS_CG_ESDHC	KINETIS_MKCG(2, 17)	/* SIM_SCGC3[17] */
-/* SPI */
-#define KINETIS_CG_SPI0		KINETIS_MKCG(5, 12)	/* SIM_SCGC6[12] */
-#define KINETIS_CG_SPI1		KINETIS_MKCG(5, 13)	/* SIM_SCGC6[13] */
-#define KINETIS_CG_SPI2		KINETIS_MKCG(2, 12)	/* SIM_SCGC3[12] */
+#define KINETIS_CG_OSC1		KINETIS_MKCG(0, 5)
+#define KINETIS_CG_UART4	KINETIS_MKCG(0, 10)
+#define KINETIS_CG_UART5	KINETIS_MKCG(0, 11)
+#define KINETIS_CG_ENET		KINETIS_MKCG(1, 0)
+#define KINETIS_CG_DAC0		KINETIS_MKCG(1, 12)
+#define KINETIS_CG_DAC1		KINETIS_MKCG(1, 13)
+#define KINETIS_CG_RNGA		KINETIS_MKCG(2, 0)
+#define KINETIS_CG_FLEXCAN1	KINETIS_MKCG(2, 4)
+#define KINETIS_CG_NFC		KINETIS_MKCG(2, 8)
+#define KINETIS_CG_SPI2		KINETIS_MKCG(2, 12)
+#define KINETIS_CG_DDR		KINETIS_MKCG(2, 14)
+#define KINETIS_CG_SAI1		KINETIS_MKCG(2, 15)
+#define KINETIS_CG_ESDHC	KINETIS_MKCG(2, 17)
+#define KINETIS_CG_LCDC		KINETIS_MKCG(2, 22)
+#define KINETIS_CG_FTM2		KINETIS_MKCG(2, 24)
+#define KINETIS_CG_FTM3		KINETIS_MKCG(2, 25)
+#define KINETIS_CG_ADC1		KINETIS_MKCG(2, 27)
+#define KINETIS_CG_ADC3		KINETIS_MKCG(2, 28)
+#define KINETIS_CG_EWM		KINETIS_MKCG(3, 1)
+#define KINETIS_CG_CMT		KINETIS_MKCG(3, 2)
+#define KINETIS_CG_I2C0		KINETIS_MKCG(3, 6)
+#define KINETIS_CG_I2C1		KINETIS_MKCG(3, 7)
+#define KINETIS_CG_UART0	KINETIS_MKCG(3, 10)
+#define KINETIS_CG_UART1	KINETIS_MKCG(3, 11)
+#define KINETIS_CG_UART2	KINETIS_MKCG(3, 12)
+#define KINETIS_CG_UART3	KINETIS_MKCG(3, 13)
+#define KINETIS_CG_USBFS	KINETIS_MKCG(3, 18)
+#define KINETIS_CG_CMP		KINETIS_MKCG(3, 19)
+#define KINETIS_CG_VREF		KINETIS_MKCG(3, 20)
+#define KINETIS_CG_LLWU		KINETIS_MKCG(3, 28)
+#define KINETIS_CG_LPTIMER	KINETIS_MKCG(4, 0)
+#define KINETIS_CG_REGFILE	KINETIS_MKCG(4, 1)
+#define KINETIS_CG_DRYICE	KINETIS_MKCG(4, 2)
+#define KINETIS_CG_DRYICESECREG	KINETIS_MKCG(4, 3)
+#define KINETIS_CG_TSI		KINETIS_MKCG(4, 5)
+#define KINETIS_CG_PORTA	KINETIS_MKCG(4, 9)
+#define KINETIS_CG_PORTB	KINETIS_MKCG(4, 10)
+#define KINETIS_CG_PORTC	KINETIS_MKCG(4, 11)
+#define KINETIS_CG_PORTD	KINETIS_MKCG(4, 12)
+#define KINETIS_CG_PORTE	KINETIS_MKCG(4, 13)
+#define KINETIS_CG_PORTF	KINETIS_MKCG(4, 14)
+#define KINETIS_CG_DMAMUX0	KINETIS_MKCG(5, 1)
+#define KINETIS_CG_DMAMUX1	KINETIS_MKCG(5, 2)
+#define KINETIS_CG_FLEXCAN0	KINETIS_MKCG(5, 4)
+#define KINETIS_CG_SPI0		KINETIS_MKCG(5, 12)
+#define KINETIS_CG_SPI1		KINETIS_MKCG(5, 13)
+#define KINETIS_CG_SAI0		KINETIS_MKCG(5, 15)
+#define KINETIS_CG_CRC		KINETIS_MKCG(5, 18)
+#define KINETIS_CG_USBHS	KINETIS_MKCG(5, 20)
+#define KINETIS_CG_USBDCD	KINETIS_MKCG(5, 21)
+#define KINETIS_CG_PDB		KINETIS_MKCG(5, 22)
+#define KINETIS_CG_PIT		KINETIS_MKCG(5, 23)
+#define KINETIS_CG_FTM0		KINETIS_MKCG(5, 24)
+#define KINETIS_CG_FTM1		KINETIS_MKCG(5, 25)
+#define KINETIS_CG_ADC0		KINETIS_MKCG(5, 27)
+#define KINETIS_CG_ADC2		KINETIS_MKCG(5, 28)
+#define KINETIS_CG_RTC		KINETIS_MKCG(5, 29)
+#define KINETIS_CG_FLEXBUS	KINETIS_MKCG(6, 0)
+#define KINETIS_CG_DMA		KINETIS_MKCG(6, 1)
+#define KINETIS_CG_MPU 		KINETIS_MKCG(6, 2)
 
 #ifndef __ASSEMBLY__
 
@@ -100,6 +121,23 @@ typedef u32 kinetis_clock_gate_t;
  * Enable or disable the clock on a peripheral device (timers, UARTs, USB, etc)
  */
 int kinetis_periph_enable(kinetis_clock_gate_t gate, int enable);
+
+/*
+ * Disable all clocks, except for those that are absolutely required
+ * to run the OS kernel. We have to do that because firmware may
+ * have enabled some of the clocks for its own operation.
+ */
+void kinetis_periph_set_minimum(void);
+
+/*
+ * Print status of a peripheral clock gate
+ */
+void kinetis_periph_print(kinetis_clock_gate_t gate, char *name);
+
+/*
+ * Print status of all peripheral clock gates
+ */
+void kinetis_periph_print_all(void);
 
 #endif /* __ASSEMBLY__ */
 
--- arch/arm/mach-kinetis/power.c
+++ arch/arm/mach-kinetis/power.c
@@ -43,6 +43,7 @@ int kinetis_periph_enable(kinetis_clock_gate_t gate, int enable)
 	 */
 	if (KINETIS_CG_REG(gate) >= KINETIS_SIM_CG_NUMREGS ||
 	    KINETIS_CG_IDX(gate) >= KINETIS_SIM_CG_NUMBITS) {
+		printk("%s: wrong gate = %x\n", __func__, gate);
 		rv = -EINVAL;
 		goto out;
 	}
@@ -64,3 +65,169 @@ out:
 	return rv;
 }
 EXPORT_SYMBOL(kinetis_periph_enable);
+
+/*
+ * Disable all clocks, except for those that are absolutely required
+ * to run the OS kernel. We have to do that because firmware may
+ * have enabled some of the clocks for its own operation.
+ */
+void kinetis_periph_set_minimum(void)
+{
+	kinetis_periph_enable(KINETIS_CG_OSC1, 0);
+	kinetis_periph_enable(KINETIS_CG_UART4, 0);
+	kinetis_periph_enable(KINETIS_CG_UART5, 0);
+	kinetis_periph_enable(KINETIS_CG_ENET, 0);
+	kinetis_periph_enable(KINETIS_CG_DAC0, 0);
+	kinetis_periph_enable(KINETIS_CG_DAC1, 0);
+	kinetis_periph_enable(KINETIS_CG_RNGA, 0);
+	kinetis_periph_enable(KINETIS_CG_FLEXCAN1, 0);
+	kinetis_periph_enable(KINETIS_CG_NFC, 0);
+	kinetis_periph_enable(KINETIS_CG_SPI2, 0);
+	// kinetis_periph_enable(KINETIS_CG_DDR, 0);
+	kinetis_periph_enable(KINETIS_CG_SAI1, 0);
+	kinetis_periph_enable(KINETIS_CG_ESDHC, 0);
+	kinetis_periph_enable(KINETIS_CG_LCDC, 0);
+	kinetis_periph_enable(KINETIS_CG_FTM2, 0);
+	kinetis_periph_enable(KINETIS_CG_FTM3, 0);
+	kinetis_periph_enable(KINETIS_CG_ADC1, 0);
+	kinetis_periph_enable(KINETIS_CG_ADC3, 0);
+	kinetis_periph_enable(KINETIS_CG_EWM, 0);
+	kinetis_periph_enable(KINETIS_CG_CMT, 0);
+	kinetis_periph_enable(KINETIS_CG_I2C0, 0);
+	kinetis_periph_enable(KINETIS_CG_I2C1, 0);
+	kinetis_periph_enable(KINETIS_CG_UART0, 0);
+	kinetis_periph_enable(KINETIS_CG_UART1, 0);
+	kinetis_periph_enable(KINETIS_CG_UART2, 0);
+	kinetis_periph_enable(KINETIS_CG_UART3, 0);
+	kinetis_periph_enable(KINETIS_CG_USBFS, 0);
+	kinetis_periph_enable(KINETIS_CG_CMP, 0);
+	kinetis_periph_enable(KINETIS_CG_VREF, 0);
+	kinetis_periph_enable(KINETIS_CG_LLWU, 0);
+	kinetis_periph_enable(KINETIS_CG_LPTIMER, 0);
+	// kinetis_periph_enable(KINETIS_CG_REGFILE, 0);
+	kinetis_periph_enable(KINETIS_CG_DRYICE, 0);
+	kinetis_periph_enable(KINETIS_CG_DRYICESECREG, 0);
+	kinetis_periph_enable(KINETIS_CG_TSI, 0);
+	kinetis_periph_enable(KINETIS_CG_PORTA, 0);
+	kinetis_periph_enable(KINETIS_CG_PORTB, 0);
+	kinetis_periph_enable(KINETIS_CG_PORTC, 0);
+	kinetis_periph_enable(KINETIS_CG_PORTD, 0);
+	kinetis_periph_enable(KINETIS_CG_PORTE, 0);
+	kinetis_periph_enable(KINETIS_CG_PORTF, 0);
+	kinetis_periph_enable(KINETIS_CG_DMAMUX0, 0);
+	kinetis_periph_enable(KINETIS_CG_DMAMUX1, 0);
+	kinetis_periph_enable(KINETIS_CG_FLEXCAN0, 0);
+	kinetis_periph_enable(KINETIS_CG_SPI0, 0);
+	kinetis_periph_enable(KINETIS_CG_SPI1, 0);
+	kinetis_periph_enable(KINETIS_CG_SAI0, 0);
+	kinetis_periph_enable(KINETIS_CG_CRC, 0);
+	kinetis_periph_enable(KINETIS_CG_USBHS, 0);
+	kinetis_periph_enable(KINETIS_CG_USBDCD, 0);
+	kinetis_periph_enable(KINETIS_CG_PDB, 0);
+	kinetis_periph_enable(KINETIS_CG_PIT, 0);
+	kinetis_periph_enable(KINETIS_CG_FTM0, 0);
+	kinetis_periph_enable(KINETIS_CG_FTM1, 0);
+	kinetis_periph_enable(KINETIS_CG_ADC0, 0);
+	kinetis_periph_enable(KINETIS_CG_ADC2, 0);
+	kinetis_periph_enable(KINETIS_CG_RTC, 0);
+	kinetis_periph_enable(KINETIS_CG_FLEXBUS, 0);
+	kinetis_periph_enable(KINETIS_CG_DMA, 0);
+	kinetis_periph_enable(KINETIS_CG_MPU, 0);
+
+	// printk("\nAFTER:\n"); kinetis_periph_print_all();
+}
+EXPORT_SYMBOL(kinetis_periph_set_minimum);
+
+/*
+ * Print status of a peripheral clock gate
+ */
+void kinetis_periph_print(kinetis_clock_gate_t gate, char *name)
+{
+	volatile u32 *scgc;
+	u32 mask;
+
+	/*
+	 * Verify the function arguments
+	 */
+	if (KINETIS_CG_REG(gate) >= KINETIS_SIM_CG_NUMREGS ||
+	    KINETIS_CG_IDX(gate) >= KINETIS_SIM_CG_NUMBITS) {
+		goto out;
+	}
+
+	scgc = &KINETIS_SIM->scgc[KINETIS_CG_REG(gate)];
+	mask = 1 << KINETIS_CG_IDX(gate);
+
+	printk("%s=%d\n", name, (*scgc & mask) ? 1 : 0);
+
+out:
+	;
+}
+EXPORT_SYMBOL(kinetis_periph_print);
+
+/*
+ * Print status of all peripheral clock gates
+ */
+void kinetis_periph_print_all(void)
+{
+	kinetis_periph_print(KINETIS_CG_OSC1, "OSC1");
+	kinetis_periph_print(KINETIS_CG_UART4, "UART4");
+	kinetis_periph_print(KINETIS_CG_UART5, "UART5");
+	kinetis_periph_print(KINETIS_CG_ENET, "ENET");
+	kinetis_periph_print(KINETIS_CG_DAC0, "DAC0");
+	kinetis_periph_print(KINETIS_CG_DAC1, "DAC1");
+	kinetis_periph_print(KINETIS_CG_RNGA, "RNGA");
+	kinetis_periph_print(KINETIS_CG_FLEXCAN1, "FLEXCAN1");
+	kinetis_periph_print(KINETIS_CG_NFC, "NFC");
+	kinetis_periph_print(KINETIS_CG_SPI2, "SPI2");
+	kinetis_periph_print(KINETIS_CG_DDR, "DDR");
+	kinetis_periph_print(KINETIS_CG_SAI1, "SAI1");
+	kinetis_periph_print(KINETIS_CG_ESDHC, "ESDHC");
+	kinetis_periph_print(KINETIS_CG_LCDC, "LCDC");
+	kinetis_periph_print(KINETIS_CG_FTM2, "FTM2");
+	kinetis_periph_print(KINETIS_CG_FTM3, "FTM3");
+	kinetis_periph_print(KINETIS_CG_ADC1, "ADC1");
+	kinetis_periph_print(KINETIS_CG_ADC3, "ADC3");
+	kinetis_periph_print(KINETIS_CG_EWM, "EWM");
+	kinetis_periph_print(KINETIS_CG_CMT, "CMT");
+	kinetis_periph_print(KINETIS_CG_I2C0, "I2C0");
+	kinetis_periph_print(KINETIS_CG_I2C1, "I2C1");
+	kinetis_periph_print(KINETIS_CG_UART0, "UART0");
+	kinetis_periph_print(KINETIS_CG_UART1, "UART1");
+	kinetis_periph_print(KINETIS_CG_UART2, "UART2");
+	kinetis_periph_print(KINETIS_CG_UART3, "UART3");
+	kinetis_periph_print(KINETIS_CG_USBFS, "USBFS");
+	kinetis_periph_print(KINETIS_CG_CMP, "CMP");
+	kinetis_periph_print(KINETIS_CG_VREF, "VREF");
+	kinetis_periph_print(KINETIS_CG_LLWU, "LLWU");
+	kinetis_periph_print(KINETIS_CG_LPTIMER, "LPTIMER");
+	kinetis_periph_print(KINETIS_CG_REGFILE, "REGFILE");
+	kinetis_periph_print(KINETIS_CG_DRYICE, "DRYICE");
+	kinetis_periph_print(KINETIS_CG_DRYICESECREG, "DRYICESECREG");
+	kinetis_periph_print(KINETIS_CG_TSI, "TSI");
+	kinetis_periph_print(KINETIS_CG_PORTA, "PORTA");
+	kinetis_periph_print(KINETIS_CG_PORTB, "PORTB");
+	kinetis_periph_print(KINETIS_CG_PORTC, "PORTC");
+	kinetis_periph_print(KINETIS_CG_PORTD, "PORTD");
+	kinetis_periph_print(KINETIS_CG_PORTE, "PORTE");
+	kinetis_periph_print(KINETIS_CG_PORTF, "PORTF");
+	kinetis_periph_print(KINETIS_CG_DMAMUX0, "DMAMUX0");
+	kinetis_periph_print(KINETIS_CG_DMAMUX1, "DMAMUX1");
+	kinetis_periph_print(KINETIS_CG_FLEXCAN0, "FLEXCAN0");
+	kinetis_periph_print(KINETIS_CG_SPI0, "SPI0");
+	kinetis_periph_print(KINETIS_CG_SPI1, "SPI1");
+	kinetis_periph_print(KINETIS_CG_SAI0, "SAI0");
+	kinetis_periph_print(KINETIS_CG_CRC, "CRC");
+	kinetis_periph_print(KINETIS_CG_USBHS, "USBHS");
+	kinetis_periph_print(KINETIS_CG_USBDCD, "USBDCD");
+	kinetis_periph_print(KINETIS_CG_PDB, "PDB");
+	kinetis_periph_print(KINETIS_CG_PIT, "PIT");
+	kinetis_periph_print(KINETIS_CG_FTM0, "FTM0");
+	kinetis_periph_print(KINETIS_CG_FTM1, "FTM1");
+	kinetis_periph_print(KINETIS_CG_ADC0, "ADC0");
+	kinetis_periph_print(KINETIS_CG_ADC2, "ADC2");
+	kinetis_periph_print(KINETIS_CG_RTC, "RTC");
+	kinetis_periph_print(KINETIS_CG_FLEXBUS, "FLEXBUS");
+	kinetis_periph_print(KINETIS_CG_DMA, "DMA");
+	kinetis_periph_print(KINETIS_CG_MPU, "MPU");
+}
+EXPORT_SYMBOL(kinetis_periph_print_all);
