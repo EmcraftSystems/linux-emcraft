01926928a13f820a4f66b9efd71035460335d4fe
Sergei Poselenov <sposelenov@emcraft.com>
2017-12-10 12:16:08 +0300

    RM 2234. A2F-SOM: Support SDcard over SPI0.
    Signed-off-by: Reinhard Mötzel <rmoetzel@z-laser.de>
    Signed-off-by: Sergei Poselenov <sposelenov@emcraft.com>

--- arch/arm/mach-a2f/spi.c
+++ arch/arm/mach-a2f/spi.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <linux/device.h>
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/sysdev.h>
@@ -29,6 +31,7 @@
 #include <linux/mtd/partitions.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/flash.h>
+#include <linux/spi/mmc_spi.h>
 #include <mach/a2f.h>
 #include <mach/platform.h>
 #include <mach/clock.h>
@@ -99,6 +102,72 @@ static struct platform_device spi_a2f_dev1 = {
 
 #endif	/* CONFIG_A2F_MSS_SPI1 */
 
+
+
+#if defined (CONFIG_SDCARD_OVER_SPI0)
+static int a2f_mmc_spi_init(struct device *dev,
+	irqreturn_t (*detect_int)(int, void *), void *data)
+{
+#if 0
+	return request_irq(SPI_A2F_DEV0_IRQ, detect_int,
+		IRQF_TRIGGER_FALLING, "mmc_spi_detect", data);
+#endif
+	return 0;
+}
+
+static void a2f_mmc_spi_exit(struct device *dev, void *data)
+{
+#if 0
+	free_irq(SPI_A2F_DEV0_IRQ, data);
+#endif
+}
+
+static struct mmc_spi_platform_data a2f_mmc_spi_pdata = {
+	.init = a2f_mmc_spi_init,
+	.exit = a2f_mmc_spi_exit,
+	.detect_delay = 100 /* ms */
+};
+
+
+/* spi_board_info.controller_data for SPI slave devices,
+* copied to spi_device.platform_data ... mostly for dma tuning
+*/
+struct a2f_spi_chip {
+	u16 ctl_reg;
+	u8 enable_dma;
+	u8 bits_per_word;
+	u8 cs_change_per_word;
+	u16 cs_chg_udelay; /* Some devices require 16-bit delays */
+	u32 cs_gpio;
+	/* Value to send if no TX value is supplied, usually 0x0 or 0xFFFF */
+	u16 idle_tx_val;
+	u8 pio_interrupt; /* Enable spi data irq */
+};
+
+struct a2f_spi_chip mmc_spi_chip_info = {
+	.enable_dma = 0,
+	.bits_per_word = 8
+};
+
+
+/*
+ * SPI slave device (= SD-Card)
+ */
+static struct spi_board_info
+	a2f_spi_board_info = {
+	.modalias = "mmc_spi",
+	.max_speed_hz = 20000000,
+	.bus_num = 0,
+	.chip_select = 0,
+	.platform_data = &a2f_mmc_spi_pdata,
+	.controller_data = &mmc_spi_chip_info,
+	.mode = SPI_MODE_3,
+};
+
+#endif
+
+
+
 /*
  * Register the A2F specific SPI controllers and devices with the kernel.
  */
@@ -110,25 +179,25 @@ void __init a2f_spi_init(void)
 	/*
  	 * Pass the reference clock to the driver
  	 */
-	platform_set_drvdata(&spi_a2f_dev0, 
+	platform_set_drvdata(&spi_a2f_dev0,
 		(void *) (a2f_clock_get(CLCK_PCLK0)));
 
 	/*
 	 * Register a platform device for this interface
 	 */
-	platform_device_register(&spi_a2f_dev0);		
+	platform_device_register(&spi_a2f_dev0);
 #endif
 #if defined(CONFIG_A2F_MSS_SPI1)
 	/*
  	 * Pass the reference clock to the driver
  	 */
-	platform_set_drvdata(&spi_a2f_dev1, 
+	platform_set_drvdata(&spi_a2f_dev1,
 		(void *) (a2f_clock_get(CLCK_PCLK1)));
 
 	/*
 	 * Register a platform device for this interface
 	 */
-	platform_device_register(&spi_a2f_dev1);		
+	platform_device_register(&spi_a2f_dev1);
 #endif
 
 	/*
@@ -187,12 +256,21 @@ void __init a2f_spi_init(void)
 			.platform_data = &spi_a2f_flash_data__a2f_hoermann_brd,
 			.mode = SPI_MODE_3,
 		};
-#endif
+#endif 	/* CONFIG_MTD_M25P80 */
 
 #if defined(CONFIG_MTD_M25P80)
 		spi_register_board_info(&spi_a2f_eeprom__a2f_hoermann_brd, 1);
-#endif
+#endif	/* CONFIG_MTD_M25P80 */
 
-#endif
+#endif	/* CONFIG_A2F_MSS_SPI1 */
+	}
+	else if (p == PLATFORM_A2F500_SOM) {
+#if defined(CONFIG_A2F_MSS_SPI0)
+
+#if defined(CONFIG_SDCARD_OVER_SPI0)
+		spi_register_board_info(&a2f_spi_board_info, 1);
+#endif 	/* CONFIG_SDCARD_OVER_SPI0 */
+
+#endif	/* CONFIG_A2F_MSS_SPI0 */
 	}
 }
